ed034c1ff3d048e3c97200c746d6189010f5c794
Jonathan Silverman <jsilverman2@gmail.com>
2017-04-16 18:40:55 -0600

    ceruversion versioning system. find out the REAL version of your app.

--- src/ceruversion.cr
+++ src/ceruversion.cr
@@ -1,6 +1,23 @@
+require "http/client"
+require "json"
 require "./ceruversion/*"
 
-module Ceruversion
-  # TODO Put your code here
-  puts "ceruversion v#{VERSION}"
+module Ceruversion #MixFlame was here and we love you. https://mixflame.com
+  puts "ceruversion version calculator"
+  if ARGV.size == 0
+    puts "your version: #{APP_NAME} v#{VERSION}"
+  else
+    github_repo = ARGV.first
+    response = HTTP::Client.get "https://api.github.com/repos/#{github_repo}/contributors"
+    if response.status_code == 200
+      commit_count = 0
+      contributors = JSON.parse(response.body)
+      contributors.each do |contributor|
+        commit_count += contributor["contributions"].as_i
+      end
+      app_name = github_repo.split('/').last
+      version = commit_count.to_f / 100.to_f
+      puts "your version: #{app_name} v#{version}"
+    end
+  end
 end
--- src/ceruversion/version.cr
+++ src/ceruversion/version.cr
@@ -1,3 +1,4 @@
 module Ceruversion
-  VERSION = "v#{(`git rev-list master --count`.to_f) / 100.to_f}"
+  APP_NAME = %x{basename `git rev-parse --show-toplevel`}.strip
+  VERSION = `git rev-list master --count`.to_f / 100.to_f
 end
